4ddcd8fc75beb0cbdb2f26bb4e7c6389bc716dd2
Andriy <andrijbuluk@gmail.com>
2026-06-13 11:35:36 +0200

    Fix: aded basename for router

--- musician/urls.py
+++ musician/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from musician.views import MusicianViewSet
 
 router = routers.DefaultRouter()
-router.register(r'musician', MusicianViewSet)
+router.register(r"musician", MusicianViewSet, basename="manage")
 urlpatterns = [
     path('api/music/', include(router.urls)),
 ]
